8d1110aa3c60cbed42197a427e96c52d0fb58ba3
Andrew Nicolaou <me@andrewnicolaou.co.uk>
2019-03-20 13:29:30 +0100

    Target web for client code

--- webpack.config.js
+++ webpack.config.js
@@ -18,14 +18,11 @@ process.on("unhandledRejection", error => {
   process.exit(1);
 });
 
-module.exports = async () => {
+const createServerConfig = async function() {
   const routes = await require("./lib/routes").paths();
 
   return {
-    entry: {
-      client: config.clientEntryPath, // JS to run on the client
-      pages: config.mainEntryPath // The statically rendered site
-    },
+    entry: { pages: config.mainEntryPath },
 
     mode: isProduction ? "production" : "development",
 
@@ -37,8 +34,8 @@ module.exports = async () => {
       filename: "assets/js/[name].js",
       path: config.outputRoot,
       /* IMPORTANT!
-     * You must compile to UMD or CommonJS
-     * so it can be required in a Node context: */
+       * You must compile to UMD or CommonJS
+       * so it can be required in a Node context: */
       libraryTarget: "umd"
     },
 
@@ -90,4 +87,73 @@ module.exports = async () => {
       })
     ]
   };
+};
+
+const createClientConfig = async function() {
+  return {
+    entry: { client: config.clientEntryPath },
+    target: "web",
+
+    mode: isProduction ? "production" : "development",
+
+    // node: { __dirname: true },
+    stats: "minimal",
+
+    output: {
+      filename: "assets/js/[name].js",
+      path: config.outputRoot
+      // /* IMPORTANT!
+      //  * You must compile to UMD or CommonJS
+      //  * so it can be required in a Node context: */
+      // libraryTarget: "umd"
+    },
+
+    // optimization: {
+    //   splitChunks: {
+    //     cacheGroups: {
+    //       styles: {
+    //         name: "styles",
+    //         test: /\.css$/,
+    //         chunks: "all",
+    //         enforce: true
+    //       }
+    //     }
+    //   }
+    // },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          include: [config.libRoot, config.configPath],
+          use: ["cache-loader", "babel-loader"]
+        },
+        {
+          test: /\.css$/,
+          use: [
+            isProduction ? MiniCssExtractPlugin.loader : "style-loader",
+            "cache-loader",
+            { loader: "css-loader", options: { modules: true } },
+            "postcss-loader"
+          ]
+        }
+      ]
+    },
+
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "client-styles.css"
+      })
+    ]
+  };
+};
+
+module.exports = async () => {
+  // The statically rendered site
+  const serverConfig = await createServerConfig();
+
+  // JS to run on the client
+  const clientConfig = await createClientConfig();
+
+  return [serverConfig, clientConfig];
 };
